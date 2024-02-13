05c2c5654cb07d492971e9f6c8113bcd23ce11a0
safcsp dev <dev@test.com>
2024-02-13 21:08:14 +0400

    Add project files.

---
+++ TextEncryption/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your secret text");
+string text = Console.ReadLine();
+if (text != null)
+{
+    string textHash = BCrypt.Net.BCrypt.EnhancedHashPassword(text,13);
+    Console.Clear();
+    Console.WriteLine(textHash);
+    
+}
+else
+{
+    Console.WriteLine("Please enter you text");
+}
